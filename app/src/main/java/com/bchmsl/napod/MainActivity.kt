27379053108370eab99fd76818bdd19023296339
bachana <bachana.mosulishvili.1@btu.edu.ge>
2022-12-11 18:33:40 +0400

    Added design for few pages

--- app/src/main/java/com/bchmsl/napod/MainActivity.kt
+++ app/src/main/java/com/bchmsl/napod/MainActivity.kt
@@ -2,10 +2,34 @@ package com.bchmsl.napod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bchmsl.napod.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val navView: BottomNavigationView = binding.bottomNavigationMenu
+
+        val navController = findNavController(R.id.fragmentContainer)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment, R.id.curiosityFragment, R.id.favoritesFragment, R.id.preferencesFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
